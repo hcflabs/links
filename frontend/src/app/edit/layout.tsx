c91e93eef663e7dddbc252a2af0c877fc9ef620e
Cj Buresch <cj@csubj.io>
2023-11-01 21:44:00 -0700

    basic form is taking FORM lol (#17)

--- frontend/src/app/edit/layout.tsx
+++ frontend/src/app/edit/layout.tsx
@@ -2,21 +2,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Links Admin",
-  description: "Human Redirector",
-};
+// export const metadata: Metadata = {
+//   title: "Links Admin",
+//   description: "Human Redirector",
+// };
 
-export default function RootLayout({
-  children,
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   );
+// }
+export default function editLayout({
+  children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+  return <div>{children}</div>;
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,18 +5,6 @@ export default function Home() {
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch("/api/v1/links")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No Links Yet?</p>;
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
